84d22f6581dd99215e0ab3bcff1eaa8b0f1bf58a
samnurik <nuraligodblessyou@gmail.com>
2023-09-26 05:54:24 +0000

    new files

---
+++ Class1/key.tf
@@ -0,0 +1,8 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket_prefix = "hello"
+}
---
+++ Class1/main.tf
@@ -0,0 +1,25 @@
+resource "aws_iam_user" "lb" {
+  name = "hello"
+}
+
+resource "aws_iam_user" "lb1" {
+  name = "hello1"
+}
+
+
+resource "aws_iam_group" "developers" {
+  name = "developers"
+}
+
+
+resource "aws_iam_group_membership" "team" {
+  name = "tf-testing-group-membership"
+
+  users = [
+    aws_iam_user.lb.name,
+    aws_iam_user.lb1.name,
+  ]
+
+  group = aws_iam_group.developers.name
+}
+
