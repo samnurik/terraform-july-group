01af5900bbce874eda640fdf40e4160c40c6ed70
samnurik <nuraligodblessyou@gmail.com>
2023-09-29 02:10:03 +0000

    New files

---
+++ Class3/VPC/ec2.tf
@@ -0,0 +1,34 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  key_name = aws_key_pair.deployer.key_name
+  user_data = file("apache.sh")
+  subnet_id = aws_subnet.main1.id
+}
+output ec2 {
+  value = aws_instance.web.public_ip
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
